573f2c9f8fc1927011b17b373088d6f4b3f17164
qweabc <480674076@qq.com>
2017-11-09 21:43:12 +0800

    l

---
+++ MyFirstjob_RTC实现万年历/src/main.c
@@ -0,0 +1,181 @@
+/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
+* File Name          : main.c
+* Author             : MCD Application Team
+* Version            : V2.0.3
+* Date               : 09/22/2008
+* Description        : Main program body
+********************************************************************************
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*******************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_lib.h"
+#include "stdio.h"
+#include "time.h"
+vu32 TimeDisplay=0;
+struct tm time_now={2011,2,8,2,20,0};
+void RccInitialisation(void);
+void GpioInitialisation(void);
+void UsartInitialisation(void);
+void RtcInitialisation(void);
+void NvicInitialisation(void);
+void Time_Show(void);
+void Time_SetCalendarTime(struct tm t);
+u32 Time_ConvCalendarToUnix(struct tm t);
+struct tm Time_ConvUnixToCalendar(time_t t);
+u32 Time_GetUnixTime(void);
+void Time_SetUnixTime(time_t t);
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	RccInitialisation();
+	NvicInitialisation();
+	GpioInitialisation();
+	UsartInitialisation();
+	RtcInitialisation();
+	Time_SetCalendarTime(time_now);
+  	  while(1)
+			{
+				Time_Show();
+			}				/* add your code here ^_^. */
+}
+void RccInitialisation(void)
+{
+	ErrorStatus HSEStartUpstatus;
+	RCC_DeInit();
+	RCC_HSEConfig(RCC_HSE_ON);
+	HSEStartUpstatus = RCC_WaitForHSEStartUp();
+	if(HSEStartUpstatus == SUCCESS)
+	{
+		RCC_HCLKConfig(RCC_SYSCLK_Div1);
+		RCC_HCLKConfig(RCC_HCLK_Div1);
+    RCC_HCLKConfig(RCC_HCLK_Div2);
+    FLASH_SetLatency(FLASH_Latency_2);
+		FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+		RCC_PLLConfig(RCC_PLLSource_HSE_Div1,RCC_PLLMul_9);
+		RCC_PLLCmd(ENABLE);
+		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+		while(RCC_GetSYSCLKSource() != 0x08);
+	}
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA,ENABLE);
+}	
+void NvicInitialisation(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+	NVIC_InitStructure.NVIC_IRQChannel=RTC_IRQChannel;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+void GpioInitialisation(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+}
+void RtcInitialisation(void)
+{
+	PWR_BackupAccessCmd(ENABLE);
+	BKP_DeInit();
+	RCC_LSEConfig(RCC_LSE_ON);
+	while(RCC_GetFlagStatus(RCC_FLAG_LSERDY)==RESET);
+	RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+	RCC_RTCCLKCmd(ENABLE);
+	RTC_WaitForSynchro();
+	RTC_WaitForLastTask();
+	RTC_ITConfig(RTC_IT_SEC,ENABLE);
+	RTC_WaitForLastTask();
+	RTC_SetPrescaler(32767);
+	RTC_WaitForLastTask();
+}
+void Time_Show(void)
+{
+	u32 CurrenTime=0;
+	if(TimeDisplay)
+	{
+		CurrenTime=Time_GetUnixTime();
+		time_now=Time_ConvUnixToCalendar(CurrenTime);
+		printf("\r\nTime: %d- %d- %d, %d: %d: %d\r\n",
+		        time_now.tm_year,
+		        time_now.tm_mon,
+		        time_now.tm_mday,
+	          time_now.tm_hour,
+		        time_now.tm_min,
+		        time_now.tm_sec);
+		TimeDisplay=0;
+	}
+}
+void UsartInitialisation(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+	USART_ClockInitTypeDef USART_ClockInitStructure;
+        USART_InitStructure.USART_BaudRate=9600;
+	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
+	USART_InitStructure.USART_Parity=USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
+	USART_Init(USART1,&USART_InitStructure);
+	USART_Cmd(USART1,ENABLE);
+}
+int fputc(int ch,FILE *f)	
+{
+	USART_SendData(USART1, (u8)ch);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET);
+	return ch;
+}
+void Time_SetCalendarTime(struct tm t)
+{
+	Time_SetUnixTime(Time_ConvCalendarToUnix(t));
+	return;
+}
+u32 Time_ConvCalendarToUnix(struct tm t)
+{
+	t.tm_year -= 1900;
+	return mktime(&t);
+}
+u32 Time_GetUnixTime(void)
+{
+	return (u32)RTC_GetCounter();
+}
+struct tm Time_ConvUnixToCalendar(time_t t)
+{
+	struct tm * t_tm;
+	t_tm=localtime(&t);
+	t_tm->tm_year+=1900;
+	return *t_tm;
+}
+void Time_SetUnixTime(time_t t)
+{
+	RTC_WaitForLastTask();
+	RTC_SetCounter((u32)t);
+	RTC_WaitForLastTask();
+	return;
+}
+
+	
+	
+
+		
+	
+	
+
+/*********************************************END OF FILE**********************/
+
