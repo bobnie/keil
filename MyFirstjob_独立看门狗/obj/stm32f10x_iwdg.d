7ef30f993d7c0f58e4d95cfc806579497d1a3462
qweabc <480674076@qq.com>
2017-11-09 21:38:27 +0800

    l

---
+++ MyFirstjob_独立看门狗/obj/stm32f10x_iwdg.d
@@ -0,0 +1,6 @@
+.\obj\stm32f10x_iwdg.o: library\src\stm32f10x_iwdg.c
+.\obj\stm32f10x_iwdg.o: .\library\inc\stm32f10x_iwdg.h
+.\obj\stm32f10x_iwdg.o: .\library\inc\stm32f10x_map.h
+.\obj\stm32f10x_iwdg.o: .\interrupt\stm32f10x_conf.h
+.\obj\stm32f10x_iwdg.o: .\library\inc\stm32f10x_type.h
+.\obj\stm32f10x_iwdg.o: .\library\inc\cortexm3_macro.h
---
+++ MyFirstjob_独立看门狗/src/main.c
@@ -0,0 +1,152 @@
+/******************** (C) COPYRIGHT 2008 STMicroelectronics ********************
+* File Name          : main.c
+* Author             : MCD Application Team
+* Version            : V2.0.3
+* Date               : 09/22/2008
+* Description        : Main program body
+********************************************************************************
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*******************************************************************************/
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_lib.h"
+#include "stdio.h"
+#include "stm32f10x_it.h"
+void RccInitialisation(void); 
+void NvicInitialisation(void);
+void GpioInitialisation(void);
+void ExtiInitialisation(void);
+void SystickInitialisation(void);
+void IwdgInitialisation(void);
+void UsartInitialisation(void);
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	RccInitialisation();
+	NvicInitialisation();
+	GpioInitialisation();
+  ExtiInitialisation();
+  UsartInitialisation();
+	if(RCC_GetFlagStatus(RCC_FLAG_IWDGRST)!=RESET)
+	{
+		printf("\r\n The STM32 has been reset by IWDG \r\n");
+		RCC_ClearFlag();
+	}
+	else 
+	{
+		printf("\r\n The STM32 has't been reset by IWDG befor \r\n");
+		SystickInitialisation();
+		IwdgInitialisation();
+	}
+  	  while(1);
+	  /* add your code here ^_^. */
+}
+void RccInitialisation(void)
+{
+	ErrorStatus HSEStartUpstatus;
+	RCC_DeInit();
+	RCC_HSEConfig(RCC_HSE_ON);
+	HSEStartUpstatus = RCC_WaitForHSEStartUp();
+	if(HSEStartUpstatus == SUCCESS)
+	{
+		RCC_HCLKConfig(RCC_SYSCLK_Div1);
+		RCC_HCLKConfig(RCC_HCLK_Div1);
+    RCC_HCLKConfig(RCC_HCLK_Div2);
+    FLASH_SetLatency(FLASH_Latency_2);
+		FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+		RCC_PLLConfig(RCC_PLLSource_HSE_Div1,RCC_PLLMul_9);
+		RCC_PLLCmd(ENABLE);
+		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+		while(RCC_GetSYSCLKSource() != 0x08);
+	}
+}	
+void GpioInitialisation(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOA,GPIO_PinSource0);
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+}
+void ExtiInitialisation(void)
+{
+	EXTI_InitTypeDef EXTI_InitStructure;
+	EXTI_InitStructure.EXTI_Line=EXTI_Line0;
+	EXTI_InitStructure.EXTI_Mode=EXTI_Mode_Interrupt;
+	EXTI_InitStructure.EXTI_Trigger=EXTI_Trigger_Falling;
+	EXTI_InitStructure.EXTI_LineCmd=ENABLE;
+	EXTI_Init (&EXTI_InitStructure);
+}
+void NvicInitialisation(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	NVIC_InitStructure.NVIC_IRQChannel=EXTI0_IRQChannel;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_Init (&NVIC_InitStructure);
+	NVIC_SystemHandlerPriorityConfig(SystemHandler_SysTick,1,0);
+}
+void SystickInitialisation(void)
+{
+	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
+	SysTick_CounterCmd(SysTick_Counter_Clear);
+	SysTick_SetReload(72000*20);
+	SysTick_CounterCmd(SysTick_Counter_Enable);
+	SysTick_ITConfig(ENABLE);
+}
+void IwdgInitialisation(void)
+{
+	IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+	IWDG_SetPrescaler(IWDG_Prescaler_32);
+	IWDG_SetReload(349);
+	IWDG_ReloadCounter();
+	IWDG_Enable();
+}
+void UsartInitialisation(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+	USART_ClockInitTypeDef USART_ClockInitStructure;
+  USART_InitStructure.USART_BaudRate=9600;
+	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits=USART_StopBits_1;
+	USART_InitStructure.USART_Parity=USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
+	USART_Init(USART1,&USART_InitStructure);
+	USART_Cmd(USART1,ENABLE);
+}
+int fputc(int ch,FILE * f)
+{
+	USART_SendData(USART1, (u8)ch);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET);
+	return ch;
+}
+
+
+
+		
+	
+	
+
+/*********************************************END OF FILE**********************/
+
